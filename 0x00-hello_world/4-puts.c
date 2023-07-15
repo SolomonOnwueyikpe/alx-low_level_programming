94d3eebe14d2c9037de11a908a5d148d85c6964f
solomon onwueyikpe <solofamily2003@yahoo.com>
2023-07-14 22:06:15 -0700

    hello world task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- * main -first point
+ * main - Entry point
  *
- * return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
