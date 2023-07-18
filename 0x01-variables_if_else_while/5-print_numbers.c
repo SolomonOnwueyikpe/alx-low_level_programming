2d712a4023eebf4bf163f7301d0cc830f51637e0
solomon onwueyikpe <solofamily2003@yahoo.com>
2023-07-17 19:08:04 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - my programm entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int p = 0;
+
+	for (; p < 10 ; p++)
+		printf("%d", p);
+	putchar('\n');
+	return (0);
+}
