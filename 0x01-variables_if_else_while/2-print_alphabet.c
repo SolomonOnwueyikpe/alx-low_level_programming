c2c81f25f8d8474aa89fb096ad3ad5771e1c857c
solomon onwueyikpe <solofamily2003@yahoo.com>
2023-07-17 18:44:21 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char p;
+
+	for (p = 'a'; p <= 'z' ; p++)
+		putchar(p);
+	putchar('\n');
+	return (0);
+}
